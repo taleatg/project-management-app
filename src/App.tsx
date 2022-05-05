294ce06b07f91cf4c6116de9f128bd6d10a6d3e1
Dina Shchobova <dina.shchobova@gmail.com>
2022-05-06 02:20:30 +0300

    fix: fix Routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,47 @@ import { NotFoundPage } from './pages/NotFoundPage';
 import { LoginPage } from './pages/LoginPage';
 import { HomePage } from './pages/HomePage';
 import { EditProfilePage } from './pages/EditProfilePage';
+import { PrivateRoute } from './routing/PrivateRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<WelcomePage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route
+            index
+            element={
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
+            }
+          />
           <Route path="/welcome" element={<WelcomePage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/edit-profile" element={<EditProfilePage />} />
-          <Route path="/board" element={<BoardPage />} />
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/edit"
+            element={
+              <PrivateRoute>
+                <EditProfilePage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/board"
+            element={
+              <PrivateRoute>
+                <BoardPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
