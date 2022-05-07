f8641b51b3ba37d40071e4cff54146e6b29d172d
Dina Shchobova <dina.shchobova@gmail.com>
2022-05-08 01:40:46 +0300

    fix: add a list of boards to the HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import { Container } from '@mui/material';
+import React, { useEffect } from 'react';
+import { CircularProgress, Container } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { getBoardsList } from '../services/boardService';
+import { useAppDispatch, useAppSelector } from '../store/store';
 
 export const HomePage = () => {
+  const { token } = useAppSelector((state) => state.authReducer);
+  const { status, allBoard } = useAppSelector((state) => state.boardReducer);
+  const dispatch = useAppDispatch();
+
+  async function fetchData() {
+    await dispatch(getBoardsList(token));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [dispatch, token]);
+
   return (
     <Container maxWidth="xl">
       <h2>HomePage</h2>
-      <Link to={`/board`} className="link">
-        <div>Board 1</div>
-      </Link>
-      <Link to={`/board`} className="link">
-        <div>Board 2</div>
-      </Link>
+      {status === 'resolved' &&
+        allBoard.length !== 0 &&
+        allBoard.map((board) => (
+          <Link to={`/board`} className="link" key={board.id}>
+            <button>{board.title}</button>
+          </Link>
+        ))}
+      {status === 'rejected' && <h4>Error! Something went wrong :( </h4>}
+      {status === 'pending' && <CircularProgress />}
     </Container>
   );
 };
