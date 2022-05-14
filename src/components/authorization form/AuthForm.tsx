5f41df556662d471c2522387fa817a4ae798cecc
Dina Shchobova <dina.shchobova@gmail.com>
2022-05-14 21:02:06 +0300

    fix: add save token in cookie

--- src/components/authorization form/AuthForm.tsx
+++ src/components/authorization form/AuthForm.tsx
@@ -9,12 +9,13 @@ import { FormField } from './FormFields';
 import { BackendResponse } from './BackendResponse';
 import { SignInForm } from '../../services/interfaces';
 import './AuthForm.scss';
-import axios from 'axios';
+import { useCookies } from 'react-cookie';
 
 export const AuthForm = () => {
   const dispatch = useAppDispatch();
-  const { switchAuthorization, setToken, setUserId } = authSlice.actions;
+  const { switchAuthorization, setUserId } = authSlice.actions;
   const { handleSubmit, control, reset, setValue } = useForm<SignInForm>();
+  const [cookies, setCookie] = useCookies(['token']);
   const [authorization, setAuthorization] = useState('signin');
   const [backendErrors, setBackendErrors] = useState('');
   const { errors } = useFormState({ control });
@@ -48,8 +49,7 @@ export const AuthForm = () => {
       const decoded = parseJwt(signin.token);
       navigate('/home');
       dispatch(switchAuthorization(true));
-      dispatch(setToken(signin.token));
-      axios.defaults.headers.common['Authorization'] = `Bearer ${signin.token}`;
+      setCookie('token', signin.token, { path: '/' });
       dispatch(setUserId(decoded.userId));
     } else if (signin.message) {
       setBackendErrors(signin.message);
