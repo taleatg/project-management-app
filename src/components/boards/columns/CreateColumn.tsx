55a724786afa7642066b29ccbc7999fccf9b73f3
Dina Shchobova <dina.shchobova@gmail.com>
2022-05-18 00:52:23 +0300

    fix: fix CreateColumn madal-window

--- src/components/boards/columns/CreateColumn.tsx
+++ src/components/boards/columns/CreateColumn.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Box, Button, Typography, Modal, TextField } from '@mui/material';
-import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+import { Box, Button, Typography, Modal, TextField, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { useAppDispatch, useAppSelector } from '../../../store/store';
 import './Columns.scss';
 import { columnSlice } from '../../../store/reducers/columnSlice';
@@ -15,7 +16,7 @@ export function CreateColumn(props: { button: JSX.Element }) {
   const { id } = useAppSelector((state) => state.boardReducer.currentBoard);
   const { allColumns } = useAppSelector((state) => state.columnReducer);
   const [open, setOpen] = React.useState(false);
-  const { handleSubmit, control, reset } = useForm<ColumnType>();
+  const { handleSubmit, register, reset } = useForm<ColumnType>();
   const { addColumn } = columnSlice.actions;
   const dispatch = useAppDispatch();
 
@@ -37,46 +38,47 @@ export function CreateColumn(props: { button: JSX.Element }) {
       <Button onClick={handleOpen}>{props.button}</Button>
       <Modal
         open={open}
+        onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Box className="modal-wrapper">
-            <Typography id="modal-modal-title" variant="h5" component="h5" align="center">
-              Add column
+        <Box className="modal-wrapper" sx={{ boxShadow: 24 }}>
+          <div className="new-column__title">
+            <Typography id="modal-modal-title" variant="h6" component="p">
+              New column
             </Typography>
-            <div className="title-wrap">
-              <Typography variant="h6" component="div" gutterBottom noWrap>
-                Enter the column title:
-              </Typography>
-              <Controller
-                control={control}
-                name="title"
-                defaultValue=""
-                rules={{
-                  required: 'Enter title',
-                }}
-                render={({ field }) => (
-                  <TextField
-                    label="title"
-                    className="auth-input"
-                    fullWidth
-                    onChange={(e) => field.onChange(e)}
-                    value={field.value}
-                  />
-                )}
-              />
-            </div>
-            <div className="button-container">
-              <Button type="submit" variant="contained">
-                Add
-              </Button>
-              <Button variant="outlined" onClick={handleClose}>
-                Close
-              </Button>
-            </div>
-          </Box>
-        </form>
+            <IconButton
+              aria-label="close"
+              onClick={handleClose}
+              sx={{
+                position: 'absolute',
+                right: 8,
+                top: 8,
+                color: (theme) => theme.palette.grey[500],
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </div>
+          <form>
+            <TextField
+              {...register('title', { required: true })}
+              fullWidth
+              margin="normal"
+              sx={{ marginTop: '30px' }}
+              label="Column title"
+              variant="outlined"
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              onClick={handleSubmit(onSubmit)}
+              sx={{ marginTop: '20px' }}
+            >
+              Create
+            </Button>
+          </form>
+        </Box>
       </Modal>
     </div>
   );
