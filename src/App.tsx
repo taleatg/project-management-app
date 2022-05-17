57bd86aeb4e77d64c5c219dd67b93f365c88ddc0
taleatg <t_korbyt@mail.ru>
2022-05-17 21:31:45 +0300

    feat: add redirecti when user is logged in and tries to proceed to signin/signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { WelcomePage } from './pages/welcome-page/WelcomePage';
 import { BoardPage } from './pages/BoardPage';
 import { Layout } from './components/Layout';
@@ -18,10 +18,9 @@ import { SignupPage } from './pages/SignupPage';
 import { ProfilePage } from './pages/ProfilePage';
 
 function App() {
-  const { setUserId } = authSlice.actions;
+  const { switchAuthorization, setUserId } = authSlice.actions;
   const dispatch = useAppDispatch();
   const [cookies] = useCookies(['token', 'userId']);
-  const { switchAuthorization } = authSlice.actions;
 
   if (cookies.token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${cookies.token}`;
@@ -74,8 +73,11 @@ function App() {
               </PrivateRoute>
             }
           />
-          <Route path="/signin" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/signin" element={cookies.token ? <Navigate to="/home" /> : <LoginPage />} />
+          <Route
+            path="/signup"
+            element={cookies.token ? <Navigate to="/home" /> : <SignupPage />}
+          />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
