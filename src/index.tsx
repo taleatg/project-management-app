2068cb3fdbb60675313eb2424a3c4c03dd9ee32a
taleatg <t_korbyt@mail.ru>
2022-05-18 21:24:00 +0300

    feat: connected i18next

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { CookiesProvider } from 'react-cookie';
 import store from './store/store';
 import { Provider } from 'react-redux';
+import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
     <CookiesProvider>
+        <Suspense fallback={<div>Loading...</div>}>
       <App />
+        </Suspense>
     </CookiesProvider>
   </Provider>
 );
