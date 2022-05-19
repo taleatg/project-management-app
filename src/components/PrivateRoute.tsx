87a695cd7b72c5dd575a275654a1161452f06157
Dina Shchobova <dina.shchobova@gmail.com>
2022-05-19 21:37:24 +0300

    fix: remove unnecessary timer

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,22 +1,7 @@
 import { Navigate } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../store/store';
-import { getCookie } from '../services/authorizationService';
-import { authSlice } from '../store/reducers/authenticationSlice';
+import { useAppSelector } from '../store/store';
 
 export function PrivateRoute({ children }: { children: JSX.Element }) {
-  const { switchAuthorization } = authSlice.actions;
-  const dispatch = useAppDispatch();
-
-  let checkTokenTimer = setTimeout(function check() {
-    const token = getCookie('token');
-    if (token) {
-      checkTokenTimer = setTimeout(check, 5000);
-    } else {
-      dispatch(switchAuthorization(false));
-      clearTimeout(checkTokenTimer);
-    }
-  }, 5000);
-
   const { isAuthenticated } = useAppSelector((state) => state.authReducer);
   return isAuthenticated ? children : <Navigate to="/welcome" />;
 }
