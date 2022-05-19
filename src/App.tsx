8d7bd63b5b2b3019c94251db82dfbbbe2d70154f
Dina Shchobova <dina.shchobova@gmail.com>
2022-05-19 21:38:01 +0300

    feat: add Redirect component

--- src/App.tsx
+++ src/App.tsx
@@ -11,28 +11,18 @@ import { EditProfilePage } from './pages/EditProfilePage';
 import { useAppDispatch, useAppSelector } from './store/store';
 import { PrivateRoute } from './components/PrivateRoute';
 import './axiosConfig';
-import axios, { AxiosRequestTransformer } from 'axios';
 import { useCookies } from 'react-cookie';
 import { authSlice } from './store/reducers/authenticationSlice';
 import { SignupPage } from './pages/SignupPage';
 import { ProfilePage } from './pages/ProfilePage';
 import { getCookie } from './services/authorizationService';
-
-type CommonHeaders = {
-  [key: string]: string;
-};
+import { Redirect } from './components/Redirect';
 
 function App() {
   const { switchAuthorization, setUserId, setCurrentUserData } = authSlice.actions;
   const { isAuthenticated } = useAppSelector((state) => state.authReducer);
   const dispatch = useAppDispatch();
   const [cookies] = useCookies(['token', 'userId']);
-
-  axios.defaults.transformRequest = ((data, headers: CommonHeaders) => {
-    const token = getCookie('token');
-    (headers.common as unknown as CommonHeaders)['Authorization'] = `Bearer ${token}`;
-    return data;
-  }) as AxiosRequestTransformer;
 
   if (getCookie('token')) {
     dispatch(setUserId(cookies.userId));
@@ -44,60 +34,62 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route
-            index
-            element={
-              <PrivateRoute>
-                <HomePage />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/welcome" element={<WelcomePage />} />
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute>
-                <HomePage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/edit"
-            element={
-              <PrivateRoute>
-                <EditProfilePage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <PrivateRoute>
-                <ProfilePage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/board/:boardId/"
-            element={
-              <PrivateRoute>
-                <BoardPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/signin"
-            element={isAuthenticated ? <Navigate to="/home" /> : <LoginPage />}
-          />
-          <Route
-            path="/signup"
-            element={isAuthenticated ? <Navigate to="/home" /> : <SignupPage />}
-          />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+      <Redirect>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <PrivateRoute>
+                  <HomePage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/welcome" element={<WelcomePage />} />
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute>
+                  <HomePage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/edit"
+              element={
+                <PrivateRoute>
+                  <EditProfilePage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <ProfilePage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/board/:boardId/"
+              element={
+                <PrivateRoute>
+                  <BoardPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/signin"
+              element={isAuthenticated ? <Navigate to="/home" /> : <LoginPage />}
+            />
+            <Route
+              path="/signup"
+              element={isAuthenticated ? <Navigate to="/home" /> : <SignupPage />}
+            />
+            <Route path="*" element={<NotFoundPage />} />{' '}
+          </Route>
+        </Routes>
+      </Redirect>
     </BrowserRouter>
   );
 }
---
+++ src/components/Redirect.tsx
@@ -0,0 +1,52 @@
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../store/store';
+import { getUserById } from '../services/authorizationService';
+import { authSlice } from '../store/reducers/authenticationSlice';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+export function Redirect({ children }: { children: JSX.Element }) {
+  const { isAuthenticated, userId } = useAppSelector((state) => state.authReducer);
+  const navigate = useNavigate();
+  const { switchAuthorization } = authSlice.actions;
+  const dispatch = useAppDispatch();
+
+  // проверка при каждом запросе: если ошибка "не авторизован" - редирект и логаут
+  axios.defaults.transformResponse = (data) => {
+    if (JSON.parse(data).statusCode === 401) {
+      dispatch(switchAuthorization(false));
+      navigate('/welcome');
+    } else {
+      if (data) {
+        return JSON.parse(data);
+      }
+    }
+  };
+
+  // тестовый запрос по таймауту через 5 минут: если ошибка "не авторизован" - редирект и логаут
+  let checkTokenTimer: NodeJS.Timeout;
+
+  if (isAuthenticated) {
+    checkTokenTimer = setTimeout(check, 1000);
+  }
+
+  async function check() {
+    const response = await getUserById(userId);
+    if (response.statusCode !== 401) {
+      checkTokenTimer = setTimeout(check, 1000 * 60 * 5);
+    } else {
+      clearTimeout(checkTokenTimer);
+    }
+  }
+
+  // удаление таймаута при размонтировании компонента
+  useEffect(() => {
+    return function cleanup() {
+      if (checkTokenTimer !== undefined) {
+        clearTimeout(checkTokenTimer);
+      }
+    };
+  });
+
+  return <div>{children}</div>;
+}
