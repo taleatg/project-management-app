05965bcd1e464d952b21f6fd32d90a09c7f4f5c4
taleatg <t_korbyt@mail.ru>
2022-05-20 15:23:10 +0300

    feat: add description of the boards

--- src/components/BoardPreview.tsx
+++ src/components/BoardPreview.tsx
@@ -1,8 +1,10 @@
-import { ListItem, ListItemText } from '@mui/material';
-import React from 'react';
-import { BoardData } from '../services/interfaces';
+import { List, ListItem, ListItemText } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { BoardData, ColumnData } from '../services/interfaces';
 import { boardSlice } from '../store/reducers/boardSlice';
 import { useAppDispatch, useAppSelector } from '../store/store';
+import { getColumns } from '../services/columnService';
+import { getTasks } from '../services/taskService';
 
 interface BoardPreviewProps {
   board: BoardData;
@@ -12,6 +14,21 @@ export const BoardPreview = (props: BoardPreviewProps) => {
   const { selectedBoard } = boardSlice.actions;
   const { allBoard } = useAppSelector((state) => state.boardReducer);
   const dispatch = useAppDispatch();
+  const [columns, setColumns] = useState(0);
+  const [tasks, setTasks] = useState(0);
+
+  useEffect(() => {
+    const getCountColumns = async () => {
+      const columns = await getColumns(props.board.id);
+      const countTasks = await columns.reduce(async (count: number, column: ColumnData) => {
+        const task = (await getTasks({ boardId: props.board.id, columnId: column.id })).length;
+        return (await count) + task;
+      }, 0);
+      setColumns(columns.length);
+      setTasks(countTasks);
+    };
+    getCountColumns();
+  }, [props.board.id]);
 
   function handlerClick() {
     const clickedBoard = allBoard.find((board) => board.id === props.board.id) as BoardData;
@@ -19,8 +36,14 @@ export const BoardPreview = (props: BoardPreviewProps) => {
   }
 
   return (
-    <ListItem onClick={handlerClick}>
-      <ListItemText secondary={props.board.title} />
-    </ListItem>
+    <List onClick={handlerClick}>
+      <ListItem sx={{ p: '0 16px' }}>
+        <ListItemText primary={props.board.title} />
+      </ListItem>
+      <ListItem sx={{ p: '0 16px' }}>
+        <ListItemText secondary={`columns: ${columns}`} />
+        <ListItemText secondary={`tasks: ${tasks}`} />
+      </ListItem>
+    </List>
   );
 };
