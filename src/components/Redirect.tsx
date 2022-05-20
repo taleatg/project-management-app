71a046a3e78a6dcffcca9f6417b79cfb97f4df84
Dina Shchobova <dina.shchobova@gmail.com>
2022-05-20 14:25:12 +0300

    fix: use useRef

--- src/components/Redirect.tsx
+++ src/components/Redirect.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../store/store';
 import { getUserById } from '../services/authorizationService';
 import { authSlice } from '../store/reducers/authenticationSlice';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import axios from 'axios';
 
 export function Redirect({ children }: { children: JSX.Element }) {
@@ -10,8 +10,8 @@ export function Redirect({ children }: { children: JSX.Element }) {
   const navigate = useNavigate();
   const { switchAuthorization } = authSlice.actions;
   const dispatch = useAppDispatch();
+  const timer: { current: NodeJS.Timeout | undefined } = useRef();
 
-  // проверка при каждом запросе: если ошибка "не авторизован" - редирект и логаут
   axios.defaults.transformResponse = (data) => {
     if (data) {
       if (JSON.parse(data).statusCode === 401) {
@@ -23,30 +23,25 @@ export function Redirect({ children }: { children: JSX.Element }) {
     }
   };
 
-  // тестовый запрос по таймауту через 5 минут: если ошибка "не авторизован" - редирект и логаут
-  let checkTokenTimer: NodeJS.Timeout;
-
-  if (isAuthenticated) {
-    checkTokenTimer = setTimeout(check, 1000);
-  }
-
   async function check() {
     const response = await getUserById(userId);
     if (response.statusCode !== 401) {
-      checkTokenTimer = setTimeout(check, 1000 * 60 * 5);
+      timer.current = setTimeout(check, 1000 * 10);
     } else {
-      clearTimeout(checkTokenTimer);
+      clearTimeout(timer.current as NodeJS.Timeout);
     }
   }
 
-  // удаление таймаута при размонтировании компонента
   useEffect(() => {
-    return function cleanup() {
-      if (checkTokenTimer !== undefined) {
-        clearTimeout(checkTokenTimer);
+    if (isAuthenticated) {
+      timer.current = setTimeout(check, 1000);
+    }
+    return () => {
+      if (timer.current !== undefined) {
+        clearTimeout(timer.current as NodeJS.Timeout);
       }
     };
-  });
+  }, []);
 
   return <div>{children}</div>;
 }
