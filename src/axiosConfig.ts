fb0f976831f6781e5eb7254f4af883512aa1fd1c
taleatg <t_korbyt@mail.ru>
2022-05-22 00:54:26 +0300

    fix: update backend path

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://fast-wildwood-10188.herokuapp.com/';
+axios.defaults.baseURL = 'https://project-management-appp.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'application/json; charset=UTF-8';
 axios.defaults.headers.put['Content-Type'] = 'application/json; charset=UTF-8';
