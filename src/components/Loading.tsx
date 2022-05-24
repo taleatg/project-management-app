9d31fe029d872b30bc412b5b98267fbae4a1f593
taleatg <t_korbyt@mail.ru>
2022-05-24 19:19:07 +0300

    feat: add ne component "Loading"

---
+++ src/components/Loading.tsx
@@ -0,0 +1,18 @@
+import CircularProgress from '@mui/material/CircularProgress';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import * as React from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const Loading = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Box className="loading-wrapper">
+      <CircularProgress sx={{ m: '0 auto', color: '#484bee' }} />
+      <Typography variant="h5" component="h5" sx={{ mt: '10px', color: '#484bee' }}>
+        {t('search.loading')}
+      </Typography>
+    </Box>
+  );
+};
